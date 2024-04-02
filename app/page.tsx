ecc2cb1357efbb68193939c218fa94116bc54f29
sjpark-02 <sujeong.park@superpacman.co.kr>
2024-04-02 19:51:07 +0900

    Revert "feat: 랜딩페이지 추가"
    This reverts commit 0f74e4d51bdf4381d8e83a21956e31ec65054a0f.

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,44 @@
-import Image from 'next/image';
+import { PropertyList } from '@/components/propertyList/PropertyList';
+import { SearchForm } from '@/components/propertyList/search/SearchForm';
 
-export default function Landing() {
+import { getAddress, getApartments } from '@/lib/queries';
+import { VStack } from '@components/Stack';
+
+export default async function List({
+  searchParams,
+}: {
+  searchParams: {
+    bigCity?: string;
+    middleCity?: string;
+    littleCity?: string | string[];
+    contractTypes?: string | string[];
+    sort?: string;
+    page?: string;
+    pageSize?: string;
+  };
+}) {
+  const bigCities = await getAddress({ addressType: 'BIG_CITY' });
+  const middleCities = !!searchParams?.bigCity
+    ? await getAddress({ addressType: 'MIDDLE_CITY', bigCityAddressCode: searchParams.bigCity })
+    : [];
+  const littleCities = !!searchParams?.middleCity
+    ? await getAddress({ addressType: 'LITTLE_CITY', middleCityAddressCode: searchParams.middleCity })
+    : [];
+  const apartments = await getApartments({
+    page: searchParams.page ? +searchParams.page - 1 : 0,
+    size: searchParams.pageSize ? +searchParams.pageSize : 15,
+    sort: searchParams.sort ? [searchParams.sort] : [],
+    dongCodes: searchParams?.littleCity ? searchParams.littleCity.toString() : undefined,
+    contractTypes: searchParams?.contractTypes ? searchParams.contractTypes.toString() : undefined,
+  });
+
   return (
-    <div className="h-auto w-full">
-      <Image
-        src="/images/main.png"
-        alt="ListBank"
-        priority={true}
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
-      />
-      <Image
-        src="/images/features.png"
-        alt="ListBank"
-        priority={true}
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
-      />
-      <Image
-        src="/images/introduce.png"
-        alt="ListBank"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
-      />
-      <Image
-        src="/images/service.png"
-        alt="ListBank"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
-      />
-      <Image
-        src="/images/footer.png"
-        alt="ListBank"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
-      />
-    </div>
+    <VStack style={{ width: 'calc(100% - var(--drawer-content-width, 0))' }}>
+      <VStack className="gap-32 px-42 py-26">
+        <h1 className="font-page-title mb-12">매물 목록</h1>
+        <SearchForm data={{ bigCities, middleCities, littleCities }} />
+        <PropertyList data={apartments} />
+      </VStack>
+    </VStack>
   );
 }
